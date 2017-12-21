06e7f2215a5c77aaa95c8aa44b6b05b51b67dc21
iceiceiceiceiceice <31066905+iceiceiceiceiceice@users.noreply.github.com>
2017-12-21 11:42:22 +0700

    asd

--- views/public/static/js/an.js
+++ views/public/static/js/an.js
@@ -4,7 +4,7 @@ function readAllRequest(){
 		if (this.readyState == 4 && this.status == 200) {
 			var data,i,htmlcode,table,td,text,input;
 			data = JSON.parse(this.responseText);
-			htmlcode = '<table id="mytable">'
+			htmlcode = '<table id="mytable" class="table table-striped table-bordered table-hover table-condensed">'
 				+'<tr>'
 				+'<th style="width:5%;">ID</th><th style="width:30%;">tên công việc</th>'
 				+'<th style="width:10%;">nhóm thực hiện</th><th style="width:10%;">mức độ ưu tiên</th>'
@@ -21,14 +21,14 @@ function readAllRequest(){
 				+'<td><input id="inputStat" placeholder="Tìm kiếm" type ="text"  style="width:95%;" class="form-control" onkeyup="myFunction(this.id)"></input></td></tr>';
 				var n = data.length;
 				for(i = 0 ; i <n;i++){
-					htmlcode+="<tr>"
-							+"<td>"+data[i].ID+"</td>"
-						 +"<td>"+data[i].Des+"</td>"
-						 +"<td>"+data[i].Team+"</td>"
-						 +"<td>"+data[i].Prio+"</td>"
-						 +"<td>"+data[i].createDate+"</td>"
-						 +"<td>"+data[i].deadline+"</td>"
-						 +"<td>"+data[i].stat+"</td>"
+					htmlcode+="<tr onclick='showfunction()'>"
+							+"<td><a href='#' >"+data[i].ID+"</a></td>"
+						 +"<td><a href='#' >"+data[i].Des+"</a></td>"
+						 +"<td><a href='#'>"+data[i].Team+"</a></td>"
+						 +"<td><a href='#'>"+data[i].Prio+"</a></td>"
+						 +"<td><a href='#'>"+data[i].createDate+"</a></td>"
+						 +"<td><a href='#'>"+data[i].deadline+"</a></td>"
+						 +"<td><a href='#'>"+data[i].stat+"</a></td>"
 						 +"</tr>";
 				}
 				htmlcode+="</table>";
@@ -43,13 +43,13 @@ function readAllRequest(){
 	//data = JSON.parse(jdata);
 	
 }
-function readAllJob(){
+function readAlltask(){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 				var data,i,htmlcode,table,td,text,input;
 				data = JSON.parse(this.reponseText);
-				htmlcode = '<table id="mytable">'
+				htmlcode = '<table id="mytable" class="table table-striped table-bordered table-hover table-condensed">'
 				+'<tr>'
 				+'<th style="width:5%;">ID</th>'
 				+'<th style="width:5%;">RequestID</th>'
@@ -81,18 +81,18 @@ function readAllJob(){
 					+"<td>"+data[i].deadline+"</td>"
 					+"<td>"+data[i].stat+"</td>"
 					+"</tr>";
-	}
-	htmlcode+="</table>";
-	//return htmlcode;
-	document.getElementById("main-content").innerHTML=htmlcode;
+				}
+				htmlcode+="</table>";
+				//return htmlcode;
+				document.getElementById("main-content").innerHTML=htmlcode;
 		
-		}
-	};
-	xhttp.open("GET", "mytask.json", true);
-	xhttp.send();
-	//	var jdata ='[{"ID":"1","RequestID":"2","Des":"Sửa máy tính","Requester":"Alex","Prio":"rất cao","createDate":"2017-1-1","deadline":"2017-2-2","stat":"hoàn thành"},{"ID":"2","RequestID":"5","Des":"Kiểm tra bàn phím","Requester":"Bobby","Prio":"rất cao","createDate":"2017-1-1","deadline":"2017-2-2","stat":"hoàn thành"},{"ID":"3","RequestID":"7","Des":"Cài lại win","Requester":"Sebastian","Prio":"Trung bình","createDate":"2017-1-1","deadline":"2017-2-2","stat":"Quá hạn"},{"ID":"4","RequestID":"3","Des":"sửa máy in","Requester":"Donald","Prio":"cao","createDate":"2017-1-1","deadline":"2017-2-2","stat":"Đang thực hiện"}]'; 
+			}
+		};
+		xhttp.open("GET", "mytask.json", true);
+		xhttp.send();
+		//	var jdata ='[{"ID":"1","RequestID":"2","Des":"Sửa máy tính","Requester":"Alex","Prio":"rất cao","createDate":"2017-1-1","deadline":"2017-2-2","stat":"hoàn 	thành"},{"ID":"2","RequestID":"5","Des":"Kiểm tra bàn phím","Requester":"Bobby","Prio":"rất cao","createDate":"2017-1-1","deadline":"2017-2-2","stat":"hoàn thành"},{"ID":"3","RequestID":"7","Des":"Cài lại win","Requester":"Sebastian","Prio":"Trung bình","createDate":"2017-1-1","deadline":"2017-2-2","stat":"Quá hạn"},{"ID":"4","RequestID":"3","Des":"sửa máy in","Requester":"Donald","Prio":"cao","createDate":"2017-1-1","deadline":"2017-2-2","stat":"Đang thực hiện"}]'; 
 	
 }
-function creatRequest(){
-	
+function showfunction(){
+	document.getElementById("mytable").innerHTML="<p>ahahah</p>";
 }
